840eb320da6199fc63010c17730cd65f4bb715ac
Paulo Luiz De Souza Visbeck <paulo.visbeck@proway.treina>
2023-10-31 15:42:16 -0300

    Exercicio POO Classe Carro. Primeira Versão. Criação de atributos privados e propriedades para leitura e gravação. Criação de construtores.

---
+++ POO-ClasseCarro/ClasseCarro/Carro.cs
@@ -0,0 +1,99 @@
+﻿namespace ClasseCarro
+{
+    internal class Carro
+    {
+        //Atributos privados
+        private string placa;
+        private string marca;
+        private string modelo;
+        private string cor;
+
+        //================================================================================
+        //Como abaixo temos mais de um método construtor, dizemos que temos uma sobrecarga
+        //================================================================================
+
+        //Construtor Padrão
+        public Carro() { }
+
+        //Construtor com todos os parâmetros
+        public Carro (string placa, string marca, string modelo, string cor)
+        {
+            Placa = placa;
+            Marca = marca;
+            Modelo = modelo;
+            Cor = cor;
+        }
+
+        //Construtor com apenas dois parâmetros
+        public Carro(string placa, string modelo)
+        {
+            Placa = placa;
+            Modelo = modelo;           
+        }
+
+        //================================================================================
+        //Propriedades de acesso aos atributos privados
+        //================================================================================
+        
+        public string Placa {
+            get { return placa; }
+            set {
+                if (value.Length < 7)
+                    Console.WriteLine("Informação de placa deve ter no mínimo 7 caracteres!");
+                else
+                    placa = value;
+            } 
+        }
+
+        public string Marca
+        {
+            get { return marca; }
+            set
+            {
+                if (value.Length < 2)
+                    Console.WriteLine("Informação de marca deve ter no mínimo 2 caracteres!");
+                else
+                    marca = value;
+            }
+        }
+
+        public string Modelo
+        {
+            get { return modelo; }
+            set
+            {
+                if (value.Length < 3)
+                    Console.WriteLine("Informação de modelo deve ter no mínimo 3 caracteres!");
+                else
+                    modelo = value;
+            }
+        }
+
+        public string Cor
+        {
+            get { return cor; }
+            set
+            {
+                if (value.Length < 3)
+                    Console.WriteLine("Informação de cor deve ter no mínimo 3 caracteres!");
+                else
+                    cor = value;
+            }
+        }
+
+
+
+        // Método usando ToString
+        public override string ToString()
+        {
+            return $"Placa: {Placa}, Marca: {Marca}, Modelo: {Modelo}, Cor: {Cor}";
+        }
+
+        // Método de escrita sem o ToString
+        public void escrever()
+        {
+            Console.WriteLine($"Placa: {Placa}, Marca: {Marca}, Modelo: {Modelo}, Cor: {Cor}");
+        }
+
+    }
+}
---
+++ POO-ClasseCarro/ClasseCarro/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ClasseCarro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro01 = new Carro();
+            carro01.Placa = "MLO3922";
+            carro01.Marca = "Ford";
+            carro01.Modelo = "EcoSport";
+            carro01.Cor = "Branco";
+            //Invocando o método ToString definido dentro da classe carro
+            Console.WriteLine(carro01.ToString());
+            Console.WriteLine();
+
+            Carro carro02 = new Carro() { Placa = "MGX1156", Marca = "Fiat", Modelo = "Pulse", Cor = "Vermelho" };
+            //Acessando outro método de escrita dos atributos
+            carro02.escrever();
+            Console.WriteLine();
+
+            //Instanciando o objeto e já passando parâmetros
+            Carro carro03 = new Carro("PLS1207", "Honda", "Civic", "Preto");
+            //Acessando outro método de escrita dos atributos
+            carro03.escrever();
+            Console.WriteLine();
+            Console.ReadKey();
+
+            
+        }
+    }
+}
