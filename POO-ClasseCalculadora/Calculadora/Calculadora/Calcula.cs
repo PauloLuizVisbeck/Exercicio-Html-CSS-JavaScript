01fcd8cbe2fa80122b0432b6ba8c21e5b668b1b9
Paulo Luiz De Souza Visbeck <paulo.visbeck@proway.treina>
2023-10-31 17:12:35 -0300

    Exercicio POO Classe Calculadora. Primeira Versão. Criação de atributos privados e propriedades para leitura e gravação. Criação de construtores. Criação de métodos para manipulação dos atributos.

---
+++ POO-ClasseCalculadora/Calculadora/Calculadora/Calcula.cs
@@ -0,0 +1,67 @@
+﻿namespace Calculadora
+{
+    internal class Calcula
+    {
+        private double num1;
+        private double num2;
+
+        //Construtor padrão (se tiver apenas ele de construtor, não precisa implementá-lo)
+        public Calcula() { }
+
+        //Construtor com dois parâmetros
+        public Calcula(double num1, double num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+
+        //Propriedade para leitura e escrita no atributo privado num1
+        public double Num1
+        {
+            get { return num1; }
+            set
+            {
+                if (value <= 0)
+                    Console.WriteLine("Informe um valor maior que zero!");
+                else
+                    num1 = value;
+            }
+        }
+
+        //Propriedade para leitura e escrita no atributo privado num2
+        public double Num2
+        {
+            get { return num2; }
+            set
+            {
+                if (value <= 0)
+                    Console.WriteLine("Informe um valor maior que zero!");
+                else
+                    num2 = value;
+            }
+        }
+        //Metodo para realizar a soma
+        public double Somar()
+        {
+            return num1 + num2;
+        }
+
+        //Metodo para realizar a subtração
+        public double Subtrair()
+        {
+            return num1 - num2;
+        }
+
+        //Metodo para realizar a multiplicação
+        public double Multiplicar()
+        {
+            return num1 * num2;
+        }
+
+        //Metodo para realizar a divisão
+        public double Dividir()
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ POO-ClasseCalculadora/Calculadora/Calculadora/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Informe um número: ");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.Write("Informe outro número: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            Calcula x = new Calcula(n1, n2);
+
+            //Console.WriteLine(x.Num1);
+            //Console.WriteLine(x.Num2);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Soma é {0}:", x.Somar());
+            Console.WriteLine("Subtração é {0}:", x.Subtrair());
+            Console.WriteLine("Multiplicação é {0}:", x.Multiplicar());
+            Console.WriteLine("Divisão é {0}:", x.Dividir().ToString("F2"));
+            Console.ReadKey();
+        }
+    }
+}
