02bf6f09ab29a56a2a3e83879634bae1dcf2b479
Paulo Luiz De Souza Visbeck <paulo.visbeck@proway.treina>
2023-11-01 14:48:45 -0300

    Exercicio POO Classe Carro usando Lista. Primeira Versão. Inclusão da classe carro com seus atributos privados.

---
+++ POO-ClasseCarroComLista/ClasseCarroComLista/ClasseCarroComLista/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClasseCarroComLista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
