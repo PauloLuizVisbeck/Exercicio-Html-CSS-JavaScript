1b151533f450afeda7ea46280595133bb57952b5
Paulo Luiz De Souza Visbeck <paulo.visbeck@proway.treina>
2023-11-10 14:03:34 -0300

    Exercicio POO SortedList Categoria e Produto. Primeira Versão.Criadas as classes CategoriaPrtoduto, Categoria e Produto com suas propriedades autoimplementadas. Na classe Categoria Produto foi criado o construtor que recebe um parâmetro.

---
+++ POO-SortedListObjetoCategoriaProduto/POO-SortedListObjetoCategoriaProduto/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace POO_SortedListObjetoCategoriaProduto
+{
+    internal class CategoriaProduto
+    {
+        public Categoria categ { get; set; } // Uma propriedade que armazena informações da categoria.
+        public List<Produto> prod { get; set; } // Uma lista de produtos associada a esta categoria.
+
+        //Construtor que recebe parâmetros
+        public CategoriaProduto(Categoria cat)
+        {
+            categ = cat; // Inicializa a categoria para a instância atual.
+            prod = new List<Produto>(); // Inicializa a lista de produtos como vazia.
+        }
+    }
+
+    internal class Categoria
+    {
+        public string Id_Cat { get; set; } // Uma propriedade que armazena o ID da categoria.
+        public string Descricao_Cat { get; set; } // Uma propriedade que armazena a descrição da categoria.
+    }
+
+    internal class Produto
+    {
+        public string Id_Prod { get; set; } // Uma propriedade que armazena o ID do produto.
+        public string Descricao_Prod { get; set; } // Uma propriedade que armazena a descrição do produto.
+        public string Marca_Prod { get; set; } // Uma propriedade que armazena a marca do produto.
+        public double Preco_Prod { get; set; } // Uma propriedade que armazena o preço do produto.
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        
+    }
+}
